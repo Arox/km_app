1ff0f2653b5f3f5fbd339c1b05ce8cc8ff98edce
Arox <mailofarox@gmail.com>
2015-07-22 00:04:48 +0300

    fix sql query increment likes
    condition limit of likes is tested in sql query (add likes__lt = 10 in
    custom queryset)

--- clients/models.py
+++ clients/models.py
@@ -5,14 +5,19 @@ from django.utils.translation import ugettext_lazy as _
 import datetime
 from third_party.image_field import ResizedImageField
 # Create your models here.
+class LimitLikesQuerySet(models.QuerySet):
+    LIMIT_LIKES = 10
+    def update(self, **kwargs):
+        return super(LimitLikesQuerySet, self.filter(likes__lt = self.LIMIT_LIKES)).update(**kwargs)
+
 class Client(models.Model):
     name = models.CharField(_('name'), max_length=40, null=False)
     sername = models.CharField(_('sername'), max_length=60, null=False)
     birthday = models.DateField(_('birthday'), null=False, help_text=_('format: d.m.yyyy'))
-    #photo = models.ImageField(_('photo'), upload_to='photos/', max_length=20, null=True)
     photo = ResizedImageField(max_width=200, max_height=300, format= 'JPEG', upload_to='photos/', max_length=20, null=True)
 
-    likes = models.IntegerField(_('likes'), default=0, validators=[MaxValueValidator(10), MinValueValidator(0)])
+    likes = models.IntegerField(_('likes'), default=0, validators=[MaxValueValidator(LimitLikesQuerySet.LIMIT_LIKES), MinValueValidator(0)])
+    objects = LimitLikesQuerySet.as_manager()
 
     @property
     def age(self):
